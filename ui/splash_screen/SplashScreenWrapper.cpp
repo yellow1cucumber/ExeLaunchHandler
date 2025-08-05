03d206e4a1881bd1a3e3f2a1aab9f5d1260c44a2
yellow1cucumber <sadovskiy1n@gmail.com>
2025-08-06 00:18:59 +0500

    feat: add SplashScreenWrapper for customizable splash screen display

---
+++ ui/splash_screen/SplashScreenWrapper.cpp
@@ -0,0 +1,61 @@
+//
+// Created by User on 05.08.2025.
+//
+
+#include "SplashScreenWrapper.h"
+
+#include <qapplication.h>
+#include <qdir.h>
+
+namespace UI {
+    SplashScreenWrapper::SplashScreenWrapper(const std::wstring &backgroundImagePath) {
+        this->preparePixmap(QString::fromStdWString(backgroundImagePath));
+    }
+
+    SplashScreenWrapper::SplashScreenWrapper() {
+        const QDir currentDirectory{QDir::currentPath()};
+        const QString backgroundImagePath{currentDirectory.absoluteFilePath("resources/splash.png")};
+        this->preparePixmap(backgroundImagePath);
+    }
+
+    SplashScreenWrapper::~SplashScreenWrapper() {
+        this->hide();
+    }
+
+    void SplashScreenWrapper::preparePixmap(const QString &backgroundImagePath) {
+        QPixmap pixmap{backgroundImagePath};
+
+        if (pixmap.isNull()) {
+            this->drawDefaultPixmap(pixmap);
+        }
+
+        splash = std::make_unique<QSplashScreen>(pixmap);
+        splash->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    }
+
+    void SplashScreenWrapper::show() {
+        this->splash->show();
+        qApp->processEvents();
+        if (this->afterOpen.has_value()) {
+            this->afterOpen.value()();
+        }
+    }
+
+    void SplashScreenWrapper::hide() {
+        if (this->beforeClose.has_value()) {
+            this->beforeClose.value()();
+        }
+        this->splash->finish(nullptr);
+    }
+
+    void SplashScreenWrapper::drawDefaultPixmap(QPixmap &pixmap) {
+        pixmap = QPixmap(500, 300);
+        pixmap.fill(Qt::white);
+
+        QPainter painter(&pixmap);
+        painter.setPen(Qt::black);
+        painter.setFont(QFont("Arial", 20));
+        painter.drawText(pixmap.rect(), Qt::AlignCenter, "Loading...");
+        painter.end();
+    }
+} // UI
